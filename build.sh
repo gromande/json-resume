f774a78235f6c8a05df287e62e84ef874aa70c86
Guillermo Roman <gromande@hawk.iit.edu>
2016-06-29 14:34:33 -0500

    Merging after Sila changes

--- build.sh
+++ build.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-hackmyresume BUILD resume.json TO out/slick.html -t ../json-resume-themes/node_modules/jsonresume-theme-slick/
-
-hackmyresume BUILD resume.json TO out/elegant.html -t ../json-resume-themes/node_modules/jsonresume-theme-elegant/
-
 hackmyresume BUILD resume.json TO out/openpage.html -t ../json-resume-themes/node_modules/jsonresume-theme-onepage/
